25a18a703b00e7116053a7da5402e2e263d008e5
Muhammad Nabeel <nabeel379@yahoo.com>
2014-02-05 13:21:15 +0500

    05/02/2014

---
+++ StudentNotifier/Models/Inotifier.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentNotifier.Models
+{
+    public interface Inotifier
+    {
+        Login SignUp(Login log);
+        Login Login(String name, String pass);
+        void AddSCourse(String Course, Login log);
+    }
+}
---
+++ StudentNotifier/Scripts/logging.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+
+	$.get('http://download.templatemo.com/themes/log?id='+693503+'&oi='+386+'&ot=1&&url='+window.location, function(json){})    
+
+});
